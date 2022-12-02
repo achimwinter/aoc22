b2effed4064d6f17cd8ac5d95ff1af92c29ca35b
Achim Winter <achim.winter95@gmail.com>
2022-12-02 20:59:28 +0100

    Add Day1

--- src/day0/day0.spec.ts
+++ src/day0/day0.spec.ts
@@ -2,6 +2,10 @@ import day0 from './index';
 
 describe('On Day 0', () =>{
     it(`part1 is identity function`, ()=>{
-        expect(day0.solveForPartOne('hello')).toBe('hello');
+        expect(day0.solveForPartOne('1000\n1000\n1000\n\n1000\n1000')).toBe('3000');
+    })
+
+    it(`part2 is identity function`, ()=>{
+        expect(day0.solveForPartTwo('1000\n2000\n3000\n\n4000\n\n5000\n6000\n\n7000\n8000\n9000\n\n10000')).toBe('45000');
     })
 });
--- src/day0/index.ts
+++ src/day0/index.ts
@@ -7,11 +7,24 @@ class Day0 extends Day {
     }
 
     solveForPartOne(input: string): string {
-        return input;
+        const caloriesPerElf = this.caloriesPerElf(input);
+
+        const maxCalories = Math.max(...caloriesPerElf);
+        return maxCalories.toString();
     }
 
     solveForPartTwo(input: string): string {
-        return input;
+        const caloriesPerElf = this.caloriesPerElf(input);
+        caloriesPerElf.sort((a, b) => (b - a));
+
+        const topThreeElfCaloriesCombined = caloriesPerElf.slice(0, 3).reduce((pv, cv) => pv + cv, 0);
+        return topThreeElfCaloriesCombined.toString();
+    }
+
+    private caloriesPerElf(input: string): number[] {
+        return input.split("\n\n")
+        .map(elf => elf.split("\n")
+        .map(x => parseInt(x)).reduce((pv, cv) => pv + cv, 0));
     }
 }
 
